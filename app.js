a98d0739920ec93f0da4d9f6b006d99c309fea9e
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-07-15 19:11:57 +0900

    0715

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 const express = require('express');
+const cookieparser = require('cookie-parser');
+const morgan = require('morgan');
+const session = require('express-session');
+const flash = require('flash');
 
 const app = express();
 
-app.listen(8080,()=>{
-    console.log("8001포트에서 서버 대기중입니다!");
+app.set('view engine', 'pug');
+app.set('views',path.join(__dirname,'views'));
+app.set('port',process.env.PORT||8001);
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname,'public')));
+app.use(expres.json());
+app.use(express.unlencoded({extended:false}));
+app.use(cookieparser('nodebirdsecret'));
+app.use(session({
+    resave: false,
+    saveUnitialized:false,
+    secret:'nodebirdsecret',
+    cookie:{
+        httpOnly:true,
+        secure:false,
+    },
+}));
+app.use(flash());
+
+
+app.listen(app.get('port'),()=>{
+    console.log(`${app.get('port')}8001포트에서 서버 대기중입니다!`);
 });
