a17fd4f6aeea0c626525e5c827f7d67ab4c97e7e
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-07-17 19:22:05 +0900

    0717

--- models/posts.js
+++ models/posts.js
@@ -1,5 +1,15 @@
 module.exports = (sequelize,DataTypes)=>{
     return seqeulize.define('posts',{
-
-    });
+        content:{
+            type:DataTypes.STRING(140),
+            allowNull: false,
+        },
+        img:{
+            type:DataTypes.STRING(200),
+            allowNull:true,
+        },
+    },{
+        timestamps:true,
+        paranoid:true,
+    })
 };
--- models/user.js
+++ models/user.js
@@ -1,17 +1,28 @@
 module.exports = (sequelize,DataTypes)=>{
     return sequelize.define('user',{
         email:{
-
+            type: DataTypes.STRING(40),
+            allowNull: false,
+            unique:true,
         },
         nick:{
-
+            type:DataTypes.STRING(15),
+            allowNull: false,
         },
         password:{
-
+            type:DataTypes.STRING(100),
+        },
+        provider:{
+            type:DataTypes.STRING(10),
+            allowNull:true,
+            defaultValue:'local',
         },
         snsId:{
-
+            type:DataTypes.STRING(30),
+            allowNull:true,
         },
-
+    },{
+        timestamps:true, //시퀄라이즈가 자동으로 수정일과 row 생성일을 기록해줍니다.
+        paranoid:true, //삭제일을 시퀄라이즈가 자동으로 기록해준다.(데이터 복구가능)
     })
 };
