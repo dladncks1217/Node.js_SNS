fce4845aad7f482ce85e64458dc5be7b9adee9a4
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-07-18 21:00:55 +0900

    0718

--- passport/index.js
+++ passport/index.js
@@ -1,3 +1,9 @@
 const local = require('../passport/localStrategy');
 const kakao = require('../passport/kakaoStrategy');
 
+module.exports = (passport)=>{
+    
+    
+    local(passport);
+    kakao(passport);
+};
