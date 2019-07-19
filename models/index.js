74aa90b434b88760f2a22d24dc477b81a333109d
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-07-20 01:16:46 +0900

    0720

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config');
 const db = {};
 
-const sequelize = new Sequeilze(
+const sequelize = new Sequelize(
   config.database, config.username, config.password, config,
 );
 
