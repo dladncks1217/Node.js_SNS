727c7c0e51c967c7945312249ae4702822d5796f
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-07-22 00:35:36 +0900

    index 라우터

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const {isLoggedIn, isNotLoggedIn } = require('./middlewares');
 
 //회원가입 페이지
-router.get('/join',(req,res)=>{
+router.get('/join',isLoggedIn,(req,res)=>{ // 회원가입 안한사람은 프로필 안뜨도록
     res.render('join',{
         title:'회원가입 - NodeBird',
-        user:null,
+        user:req.user,
         joinError:req.flash('joinError'),//connect flash 모듈로 에러를 넣었다.
     });
 });
 //프로필 페이지
-router.get('/profile',(req,res)=>{
+router.get('/profile',isNotLoggedIn, (req,res)=>{ // 로그인 한사람은 로그인창 안뜨도록
     res.render('profile',{title:'내 정보 - NodeBird',user:null});
 });
  
@@ -19,7 +20,7 @@ router.get('/',(req,res,next)=>{
      res.render('main',{
          title:'NodeBird',
          twits:[],
-         user:null,
+         user:req.user,
          loginError:req.flash('loginError'), //connect flash 모듈로 에러를 넣었다.
      });
 });
