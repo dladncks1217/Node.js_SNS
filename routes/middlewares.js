161ea33d7dff5cf98f849bb849f103d6b17a582e
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-07-22 17:18:55 +0900

    0722 로그인 확인여부라우터 코드수정

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -6,7 +6,7 @@ exports.isLoggedIn = (req,res,next)=>{
     };
 }
 
-exports.isNotLoggedIn = req,res,next=>{
+exports.isNotLoggedIn = (req,res,next)=>{
     if(!req.isAuthenticated()){
         next();
     }else{
