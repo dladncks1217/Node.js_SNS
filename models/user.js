145d561e5a5e5261866a761b263342c342a80502
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-07-30 01:06:43 +0900

    0730 user모델 코드수정

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ module.exports = (sequelize,DataTypes)=>(
     sequelize.define('user',{
         email:{
             type: DataTypes.STRING(40),
-            allowNull: false,
+            allowNull: true,
             unique:true,
         },
         nick:{
@@ -11,6 +11,7 @@ module.exports = (sequelize,DataTypes)=>(
         },
         password:{ 
             type:DataTypes.STRING(100),
+            allowNull:true,
         },
         provider:{
             type:DataTypes.STRING(10),
