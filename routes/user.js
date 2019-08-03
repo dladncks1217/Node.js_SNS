4ba5429135c8d7a016518fc05cc69b4149f43547
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-08-03 22:09:58 +0900

    0803 팔로워라우터 코드수정

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,15 @@
 const express = require('express');
 
-const {User} = require('../models');
 const { isLoggedIn } = require('./middlewares');
+const {User} = require('../models');
+
 
 const router = express.Router();
 
 
 router.post('/:id/follow',isLoggedIn , async(req,res,next)=>{
     try{
-    const user = await user.findOne({where: { id:req.user.id }});
+    const user = await User.findOne({where: { id:req.user.id }});
     await user.addFollowing(parseInt(req.params.id,10));
     res.send('success');
     }catch(error){
