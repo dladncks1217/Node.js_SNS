f56cc95f300d60c9805cd8a767d43b83943d375f
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-08-04 14:55:18 +0900

    0804 언팔로우 추가

--- routes/user.js
+++ routes/user.js
@@ -16,6 +16,16 @@ router.post('/:id/follow',isLoggedIn , async(req,res,next)=>{
         console.error(error);
         next(error);
     }
-})
+});
+router.post('/:id/unfollow',isLoggedIn , async(req,res,next)=>{
+    try{
+    const user = await User.findOne({where: { id:req.user.id }});
+    await user.removeFollowing(parseInt(req.params.id,10));
+    res.send('success');
+    }catch(error){
+        console.error(error);
+        next(error);
+    }
+});
 
 module.exports = router;
