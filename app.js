c0b8401dff998d1620d8a94f1278b6e01d2518f4
dladncks1217 <dlaxodud1217@gmail.com>
2021-08-08 14:44:04 +0900

    Update dependencies

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const localpassport = require('passport-local');
 const kakaopassport = require('passport-kakao');
 const bcrypt = require('bcryptjs');
 const flash = require('connect-flash');
-const helmet = require('helmet');
-const hpp = require('hpp'); // hpp공격 방어해주는 패키지.
 const passportConfig = require('./passport'); //passport의 index연결
 const favicon = require('serve-favicon');
 
@@ -17,7 +15,6 @@ const IndexRouter = require('./routes/index');
 const authRouter = require('./routes/auth');
 const postRouter = require('./routes/post');
 const UserRouter = require('./routes/user');
-const logger = require('./logger');
 
 const {sequelize} = require('./models');
   
@@ -33,19 +30,14 @@ app.set('views',path.join(__dirname,'views'));
 app.set('port',process.env.PORT||8001);
 
 
-if(process.env.NODE_ENV ==='production'){
-    app.use(morgan('combined'));
-}else{// development
-    app.use(morgan('dev'));
-}
-
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname,'public'))); // /main.css
 app.use('/img', express.static(path.join(__dirname,'uploads'))); // /img/abc.png
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cookieparser(process.env.COOKIE_SECRET));
-const sessionOption = {
+app.use(session({
     resave: false,
     saveUnitialized:false,
     secret:process.env.COOKIE_SECRET,
@@ -53,12 +45,7 @@ const sessionOption = {
         httpOnly:true,
         secure:false,
     },
-}
-if(process.env.NODE_ENV === 'production'){
-    sessionOption.proxy=true; // 프록시서버 두기 가능.
-    sessionOption.cookie.secure=true; // https사용 가능
-}
-app.use(session(sessionOption));
+}));
 app.use(flash());
 
 app.use(passport.initialize()); //passport는 세션을 사용해야하기에 반드시 express-session밑에 passport쪽 미들웨어 연결.
@@ -74,8 +61,6 @@ app.use('/user',UserRouter);
 app.use((req,res,next)=>{
     const err = new Error('NOT FOUND');
     err.status = 404;
-    logger.info('hello'); // console.info 대체.
-    logger.error('err.message'); // console.error 대체
     next(err);
 });
 
